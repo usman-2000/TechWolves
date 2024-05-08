4014ac77fdad751f73087aae529669bbcdc498dc
usman-2000 <usmanrahim2000@gmail.com>
2024-05-09 00:32:22 +0500

    Tailwind css added

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h3>Usman Rahim</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
